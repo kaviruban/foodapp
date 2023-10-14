8d2fabf7608f1eae0b55d5ec4182b39c87896d91
Kaviruban Mohanasundaram <ruban7154@gmail.com>
2023-10-14 08:56:07 +0530

    initial

--- src/Components/AddRecipeCmp/AddRecipeComp.jsx
+++ src/Components/AddRecipeCmp/AddRecipeComp.jsx
@@ -2,31 +2,26 @@ import React, { useState } from "react";
 import "./AddReceipeCompStyl.css";
 import Dropdown from "../DropDownBox/DropDownComp";
 import { useDispatch, useSelector } from "react-redux";
-import  { addRecipe } from "../../Features/Send/SendSlice";
+import { addRecipe } from "../../Features/Send/SendSlice";
 import { useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
-// var count  = 12;
 function SubmitRecipeComp() {
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
 
-  // ...  
 
-  const[recipeTitle , setRecipeTitle] = useState("");
-  const[recipeDescription , setRecipeDescription] = useState("");
-  const[preparationTime , setPreparationTime] = useState("");
-  const[servings , setServings] = useState("");
-  const[calories , setCalories] = useState("");
+  const [recipeTitle, setRecipeTitle] = useState("");
+  const [recipeDescription, setRecipeDescription] = useState("");
+  const [preparationTime, setPreparationTime] = useState("");
+  const [servings, setServings] = useState("");
+  const [calories, setCalories] = useState("");
 
-  
   const [ingredients, setIngredients] = useState([{ name: "", quantity: "" }]);
   const [descriptions, setDescriptions] = useState([""]);
-
-
 
   const addIngredientField = () => {
     setIngredients([...ingredients, { name: "", quantity: "" }]);
@@ -60,22 +55,18 @@ function SubmitRecipeComp() {
     setDescriptions(updatedDescriptions);
   };
 
-
-
   function HandleSubmit() {
-    
-    
     // Collect all the data you want to send
     const recipeData = {
       // id : count,
       receipeName: recipeTitle,
       recipeDescription: recipeDescription,
       card_img: require("../../Assets/Images/recipeThumb-02.3661f369.jpg"),
-      rating : "3",
+      rating: "3",
       duration: preparationTime,
       Ingredients: [ingredients],
       Directions: [descriptions],
-      
+
       // servings: servings,
       // calories: calories,
     };
@@ -83,7 +74,7 @@ function SubmitRecipeComp() {
     console.log(recipeData);
     // Dispatch the action with the recipe data
     dispatch(addRecipe(recipeData));
-    toast.success('Successfully Recipe Added !', {
+    toast.success("Successfully Recipe Added !", {
       position: "top-center",
       autoClose: 2000,
       hideProgressBar: false,
@@ -92,17 +83,13 @@ function SubmitRecipeComp() {
       draggable: false,
       progress: undefined,
       theme: "light",
-      });
-      setTimeout(() => {
-        navigate("/home")
-      } , 2000)
-    
-    
+    });
+    setTimeout(() => {
+      navigate("/home");
+    }, 1000);
+
     // alert("Recipe SuccessFully Added")
   }
-
-
-
 
   return (
     <div>
@@ -116,124 +103,141 @@ function SubmitRecipeComp() {
         </div>
       </div>
 
-        <div className="input-container">
-          <div className="recipe-title-container">
-            <label htmlFor="">Recipe Title</label>
-            <input
-              type="text"
-              className="store-recipe-title"
-              placeholder="Enter the title"
-              onChange={(e) => {setRecipeTitle(e.target.value)}}
-              value = {recipeTitle}
-            />
-          </div>
-
-          <div className="recipe-description-container">
-            <label htmlFor="">Recipe Description</label>
-            <textarea
-              className="recipe-description-box"
-              id="recipe-description-box"
-              type="text"
-              name=""
-              cols="5"
-              maxLength="500"
-              placeholder="Type the description here"
-              onChange={(e) => {setRecipeDescription(e.target.value)}}
-              value = {recipeDescription}
-            ></textarea>
-          </div>
-
-          <div className="choose-category-container">
-            <label htmlFor="">Choose Category</label>
-            <Dropdown />
-          </div>
-
-          <div className="ingredients-box">
-            <p>Ingredients</p>
-            {ingredients.map((ingredient, index) => (
-              <div className="ingredients-container" key={index}>
-                <input
-                  type="text"
-                  placeholder="Name of the ingredient"
-                  name="name"
-                  value={ingredient.name}
-                  // onChange = {HandleChange}
-                  onChange={(event) => handleIngredientChange(index, event)}
-                />
-                <input
-                  type="text"
-                  placeholder="Quantity(eg.1/2)"
-                  name="quantity"
-                  value={ingredient.quantity}
-                  // onChange = {HandleChange}
-                  onChange={(event) => handleIngredientChange(index, event)}
-                />
-                <button
-                  className="remove-btn"
-                  onClick={() => removeIngredientField(index)}
-                >
-                  X
-                </button>
-              </div>
-            ))}
-            <button className="button" onClick={addIngredientField}>
-              ADD NEW INGREDIENT
-            </button>
-          </div>
-
-          <div className="description-box">
-            <p>Description</p>
-            {descriptions.map((description, index) => (
-              <div className="description-container" key={index}>
-                <input
-                  type="text"
-                  placeholder="Type the description here"
-                  value={description}
-                  // onChange = {HandleChange}
-                  onChange={(event) => handleDescriptionChange(index, event)}
-                />
-                <button
-                  className="remove-btn"
-                  onClick={() => removeDescriptionField(index)}
-                >
-                  X
-                </button>
-              </div>
-            ))}
-            <button className="button" onClick={addDescriptionField}>
-              ADD NEW DESCRIPTION
-            </button>
-          </div>
-
-          <div className="preparation-time-container">
-            <p>Preparation Time</p>
-            <input type="text" placeholder="(eg.30mins)"
-            onChange={(e) => {setPreparationTime(e.target.value)}}
-            value = {preparationTime}
-            />
-          </div>
-
-          <div className="servings-container">
-            <p>Servings</p>
-            <input type="text" placeholder="(eg.4 servings)"
-            onChange={(e) => {setServings(e.target.value)}}
-            value = {servings} 
-            />
-          </div>
-
-          <div className="calories-container">
-            <p>Calories</p>
-            <input type="text" placeholder="(eg.634 kcal)"
-            onChange={(e) => {setCalories(e.target.value)}}
-            value = {calories} 
-            />
-          </div>
-
-          <button className="button" onClick={HandleSubmit} >Submit</button>
- 
+      <div className="input-container">
+        <div className="recipe-title-container">
+          <label htmlFor="">Recipe Title</label>
+          <input
+            type="text"
+            className="store-recipe-title"
+            placeholder="Enter the title"
+            onChange={(e) => {
+              setRecipeTitle(e.target.value);
+            }}
+            value={recipeTitle}
+          />
         </div>
+
+        <div className="recipe-description-container">
+          <label htmlFor="">Recipe Description</label>
+          <textarea
+            className="recipe-description-box"
+            id="recipe-description-box"
+            type="text"
+            name=""
+            cols="5"
+            maxLength="500"
+            placeholder="Type the description here"
+            onChange={(e) => {
+              setRecipeDescription(e.target.value);
+            }}
+            value={recipeDescription}
+          ></textarea>
+        </div>
+
+        <div className="choose-category-container">
+          <label htmlFor="">Choose Category</label>
+          <Dropdown />
+        </div>
+
+        <div className="ingredients-box">
+          <p>Ingredients</p>
+          {ingredients.map((ingredient, index) => (
+            <div className="ingredients-container" key={index}>
+              <input
+                type="text"
+                placeholder="Name of the ingredient"
+                name="name"
+                value={ingredient.name}
+                // onChange = {HandleChange}
+                onChange={(event) => handleIngredientChange(index, event)}
+              />
+              <input
+                type="text"
+                placeholder="Quantity(eg.1/2)"
+                name="quantity"
+                value={ingredient.quantity}
+                // onChange = {HandleChange}
+                onChange={(event) => handleIngredientChange(index, event)}
+              />
+              <button
+                className="remove-btn"
+                onClick={() => removeIngredientField(index)}
+              >
+                X
+              </button>
+            </div>
+          ))}
+          <button className="button" onClick={addIngredientField}>
+            ADD NEW INGREDIENT
+          </button>
+        </div>
+
+        <div className="description-box">
+          <p>Description</p>
+          {descriptions.map((description, index) => (
+            <div className="description-container" key={index}>
+              <input
+                type="text"
+                placeholder="Type the description here"
+                value={description}
+                // onChange = {HandleChange}
+                onChange={(event) => handleDescriptionChange(index, event)}
+              />
+              <button
+                className="remove-btn"
+                onClick={() => removeDescriptionField(index)}
+              >
+                X
+              </button>
+            </div>
+          ))}
+          <button className="button" onClick={addDescriptionField}>
+            ADD NEW DESCRIPTION
+          </button>
+        </div>
+
+        <div className="preparation-time-container">
+          <p>Preparation Time</p>
+          <input
+            type="text"
+            placeholder="(eg.30mins)"
+            onChange={(e) => {
+              setPreparationTime(e.target.value);
+            }}
+            value={preparationTime}
+          />
+        </div>
+
+        <div className="servings-container">
+          <p>Servings</p>
+          <input
+            type="text"
+            placeholder="(eg.4 servings)"
+            onChange={(e) => {
+              setServings(e.target.value);
+            }}
+            value={servings}
+          />
+        </div>
+
+        <div className="calories-container">
+          <p>Calories</p>
+          <input
+            type="text"
+            placeholder="(eg.634 kcal)"
+            onChange={(e) => {
+              setCalories(e.target.value);
+            }}
+            value={calories}
+          />
+        </div>
+
+        <button className="button" onClick={HandleSubmit}>
+          Submit
+        </button>
       </div>
+    </div>
   );
-};
+}
 
 export default SubmitRecipeComp;
--- src/Components/Slider/SliderComp.jsx
+++ src/Components/Slider/SliderComp.jsx
@@ -1,16 +1,104 @@
-import React from 'react'
-import BgImgOne from "../../Assets/Images/sliderA_01.7f16b740.jpg"
-import BGImgTwo from "../../Assets/Images/sliderA_02.5936693f.jpg"
-import BgImgThree from "../../Assets/Images/sliderA_01.7f16b740.jpg"
-import BGImgFour from "../../Assets/Images/sliderA_02.5936693f.jpg"
+import BgImgOne from "../../Assets/Images/sliderA_01.7f16b740.jpg";
+import BgImgTwo from "../../Assets/Images/sliderA_02.5936693f.jpg";
+import BgImgThree from "../../Assets/Images/sliderA_01.7f16b740.jpg";
+import BgImgFour from "../../Assets/Images/sliderA_02.5936693f.jpg";
+import { useState ,useEffect} from "react";
+import './SliderComp.css'
 
 function SliderComp() {
-    const SliderImages = [BgImgOne , BGImgTwo , BgImgThree , BGImgFour]
-    return SliderImages.map((data , index) =>{
-      return (
-          <img src={data} alt="nothing-just-a-slider-image" />
-        );
-    });
+  const [Image, setImage] = useState({});
+  const sliderData = [
+    {
+      id: 0,
+      receipeName: "Mexican grilled Corn Recipe",
+      recipeDescription:
+        "Maecenas in massa eget urna ullamcorper pharetra. Curabitur laoreet scelerisque bibendum. Aenean ullamcorper neque ac tristique semper. Phasellus enim mauris, mollis vulputate blandit in, ornare sed leo.",
+      card_img: BgImgOne,
+      rating: "3",
+      duration: "1 Hr",
+      Ingredients: [
+        {
+          name: "pounds cubed beef stew meat",
+          quantity: "1",
+        },
+        {
+          name: "pounds cubed beef stew meat",
+          quantity: "1",
+        },
+        {
+          name: "pounds cubed beef stew meat",
+          quantity: "1",
+        },
+      ],
+      Directions: [
+        "In a Dutch oven, heat oil over medium heat until hot, but not smoking. Pat the meat dry with paper towels and brown in batches, transferring the meat with a slotted spoon to a bowl as they are done.",
+
+        "In the fat remaining in the pot, cook the onions until softened, about 5 minutes",
+        ,
+        "In the fat remaining in the pot, cook the onions until softened, about 5 minutes",
+        ,
+        "In the fat remaining in the pot, cook the onions until softened, about 5 minutes",
+        ,
+      ],
+    },
+    {
+      id: 1,
+      receipeName: "Roast Chicken With Lemon Gravy",
+      recipeDescription:
+        "Maecenas in massa eget urna ullamcorper pharetra. Curabitur laoreet scelerisque bibendum. Aenean ullamcorper neque ac tristique semper. Phasellus enim mauris, mollis vulputate blandit in, ornare sed leo.",
+      card_img: BgImgTwo,
+      rating: "4",
+      duration: "10 min",
+      Ingredients: [
+        {
+          name: "pounds cubed beef stew meat",
+          quantity: "1",
+        },
+        {
+          name: "pounds cubed beef stew meat",
+          quantity: "1",
+        },
+        {
+          name: "pounds cubed beef stew meat",
+          quantity: "1",
+        },
+      ],
+      Directions: [
+        "In a Dutch oven, heat oil over medium heat until hot, but not smoking. Pat the meat dry with paper towels and brown in batches, transferring the meat with a slotted spoon to a bowl as they are done.",
+
+        "In the fat remaining in the pot, cook the onions until softened, about 5 minutes",
+        ,
+        "In the fat remaining in the pot, cook the onions until softened, about 5 minutes",
+        ,
+        "In the fat remaining in the pot, cook the onions until softened, about 5 minutes",
+        ,
+      ],
+    },
+  ];
+
+  // setImage(sliderData[0])
+  useEffect(() => {
+    setImage(sliderData[0])
+  }, []);
+  console.log(Image);
+  // const SliderImages = [BgImgOne, BgImgTwo, BgImgThree, BgImgFour];
+  // return SliderImages.map((data, index) => {
+  //   return <img src={data} alt="nothing-just-a-slider-image" />;
+  // });
+
+  return (
+    <div className="container">
+      <div className="Image-contianer">
+        {Image.receipeName}
+      </div>
+      <div className="button-container">
+        <div className="imagechanger">m</div>
+        <div className="imagechanger">r</div>
+        <div className="imagechanger">a</div>
+        <div className="imagechanger">c</div>
+      </div>
+    </div>
+  )
 }
 
-export default SliderComp
+export default SliderComp;
